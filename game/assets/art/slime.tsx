9dd8b4b3300fa8cd3edd339142ff0d55a758a3b4
o7-machinehum <ryan.cjw@gmail.com>
2026-03-01 21:11:08 +0100

    Added a monster.

---
+++ game/assets/art/slime.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="slime" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="slime.png" width="128" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="monster_name" value="slime"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ game/assets/art/tiny16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiny16" tilewidth="16" tileheight="16" tilecount="1068" columns="12">
+ <image source="tiny16.png" width="192" height="1424"/>
+</tileset>
