a3a2138ea79c3eefdcc8294826b722766c815b6c
o7-machinehum <ryan.cjw@gmail.com>
2026-03-01 22:48:19 +0100

    Worked on the map a little.

--- game/assets/art/overworld.tsx
+++ game/assets/art/overworld.tsx
@@ -1,12 +1,32 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
  <image source="overworld.png" width="640" height="576"/>
+ <tile id="22">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="44">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="45">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
@@ -46,6 +66,16 @@
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="102">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="126">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
@@ -66,6 +96,16 @@
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="142">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="166">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
@@ -82,6 +122,16 @@
   </properties>
  </tile>
  <tile id="170">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
@@ -107,6 +157,16 @@
   </properties>
  </tile>
  <tile id="214">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="222">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="224">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
@@ -447,6 +507,246 @@
   </properties>
  </tile>
  <tile id="761">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="840">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="841">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="842">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="880">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="881">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="882">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="920">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="921">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="922">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="960">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="961">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="962">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1000">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1001">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1002">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1022">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1040">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1041">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1042">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1058">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1059">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1060">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1061">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1062">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1080">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1081">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1082">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1098">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1099">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1100">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1101">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1102">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1244">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1245">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1246">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1247">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1284">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1285">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1286">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1287">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1324">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1325">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1326">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1327">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1364">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1365">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1366">
+  <properties>
+   <property name="non_walkable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1367">
   <properties>
    <property name="non_walkable" type="bool" value="true"/>
   </properties>
